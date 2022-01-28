7ce9d276eb37b497148555c8440529fa27837199
Christian Yngvesson <chrille_y@hotmail.com>
2022-01-28 01:33:30 +0100

    Get Nasa Photo from dynamic link

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,3 +1,7 @@
+// NASA API Configuration
+const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/';
+const key = 'dTWaCG83goCFI2yRG0ucxYRAx7eMOmBbyyqdzbOY';
+
 async function getData(file) {
     const response = await fetch(file);
     const data = await response.json();
@@ -12,7 +16,8 @@ async function startup() {
 async function roverMenu() {
     const rovers = await getData('api/rovers');
     for (const rover of rovers) {
-        createRoverButton(rover)
+        createRoverButton(rover);
+        getPhoto(rover);
     }
 }
 
@@ -32,4 +37,19 @@ function createRoverButton(rover) {
     })
 
     roverMenu.appendChild(roverButton)
+}
+
+async function getPhoto(rover) {
+    const roverButton = document.getElementById(rover.name);
+    const roverPhoto = document.createElement('div');
+
+    const roverPhotoImg = document.createElement('img');
+    const roverData = await getData(url + rover.url + '/photos?sol=1000&page=0&api_key=' + key);
+
+    console.log(roverData);
+    roverPhotoImg.src = roverData.photos[0].img_src;
+    console.log(roverData.photos[0]);
+
+    roverPhoto.appendChild(roverPhotoImg);
+    roverButton.appendChild(roverPhoto);
 }
