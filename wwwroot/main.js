d221650117d63c39d1bf986ff13fbe0efd61f8b8
Christian Yngvesson <chrille_y@hotmail.com>
2022-01-28 11:33:44 +0100

    Added gallery and info to rovers

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,6 +1,15 @@
+"use strict";
+
 // NASA API Configuration
 const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/';
 const key = 'dTWaCG83goCFI2yRG0ucxYRAx7eMOmBbyyqdzbOY';
+
+// Global Elements
+const main = document.getElementById('main');
+const roverMenu = document.getElementById('roverMenu');
+const roverPanel = document.getElementById('roverPanel');
+const roverInfo = document.getElementById('roverInfo');
+const roverGallery = document.getElementById('roverGallery');
 
 async function getData(file) {
     const response = await fetch(file);
@@ -13,43 +22,67 @@ async function startup() {
     console.log(rovers);
 }
 
-async function roverMenu() {
+async function createMenu() {
     const rovers = await getData('api/rovers');
     for (const rover of rovers) {
         createRoverButton(rover);
-        getPhoto(rover);
     }
 }
 
-startup();
-roverMenu();
-
 function createRoverButton(rover) {
     const roverButton = document.createElement('div');
-    const roverMenu = document.getElementById('roverMenu');
 
     roverButton.id = rover.name;
     roverButton.classList.add('roverButton');
     roverButton.textContent = rover.name;
 
-    roverButton.addEventListener('click', function () {
-        console.log(roverButton.id + " was clicked!");
-    })
+    roverButton.addEventListener('click', function (event) {
+        event.stopPropagation();
+        roverMenu.classList.add('hide');
+        roverPanel.classList.remove('hide');
+        showInfo(rover);
+        showRoverGallery(rover);
+    });
 
-    roverMenu.appendChild(roverButton)
+    roverMenu.appendChild(roverButton);
 }
 
-async function getPhoto(rover) {
-    const roverButton = document.getElementById(rover.name);
-    const roverPhoto = document.createElement('div');
+async function showInfo(rover) {
+    const roverName = document.createElement('h3');
+    roverName.textContent = rover.name;
+    roverInfo.appendChild(roverName);
 
-    const roverPhotoImg = document.createElement('img');
+    const roverDescription = document.createElement('h5');
+    roverDescription.textContent = rover.description;
+    roverInfo.appendChild(roverDescription);
+}
+
+async function showRoverGallery(rover) {
     const roverData = await getData(url + rover.url + '/photos?sol=1000&page=0&api_key=' + key);
 
-    console.log(roverData);
-    roverPhotoImg.src = roverData.photos[0].img_src;
-    console.log(roverData.photos[0]);
+    for (let index = 0; index < roverData.photos.length; index++) {
+        const element = roverData.photos[index];
 
-    roverPhoto.appendChild(roverPhotoImg);
-    roverButton.appendChild(roverPhoto);
+        const roverThumb = document.createElement('img');
+        roverThumb.alt = 'Photo';
+        roverThumb.src = element.img_src;
+        roverThumb.width = 100;
+        roverGallery.appendChild(roverThumb);
+    }
 }
+
+function removeChildren(parent) {
+    while (parent.firstElementChild) {
+        parent.firstElementChild.remove();
+    }
+}
+
+startup();
+createMenu();
+
+document.addEventListener('click', event => {
+        roverMenu.classList.remove('hide');
+        roverPanel.classList.add('hide');
+        removeChildren(roverInfo);
+        removeChildren(roverGallery);
+});
